a3f1307dc3a5c80775efeb87e71e97fe195b0c62
cchbssm <cheoc222@gmail.com>
2024-06-06 19:38:27 +0900

    Create 1551.c

---
+++ 1551.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int n, d[100010], k;
+
+
+
+
+int f(int x)
+{
+    for(int i=1; i<=n; i++)
+        if(k==d[i]) return i;
+    return -1;
+}
+int main()
+{
+  scanf("%d", &n);
+
+  for(int i=1; i<=n; i++)
+    scanf("%d", &d[i]);
+
+  scanf("%d", &k);
+  printf("%d\n", f(k));
+}
