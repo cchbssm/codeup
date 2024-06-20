119e7012aad3f8e81ef2ca62e40f7ccd27d402ce
cchbssm <cheoc222@gmail.com>
2024-06-20 12:21:49 +0900

    Create 1469 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-2.c

---
+++ 1469 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++, puts("")){
+        for(int j=0; j<n; j++) printf("%d ", n*(i+1)-j);
+        i++;
+        if(i>=n) break;
+        puts("");
+        for(int j=0; j<n; j++) printf("%d ", 1+(n*i)+j);
+    }
+    
+    return 0;
+}
