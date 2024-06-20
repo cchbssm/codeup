e48a916ec7212b753fb55ece410656dca5c129fe
cchbssm <cheoc222@gmail.com>
2024-06-20 12:01:39 +0900

    Update 1461 : [기초-배열연습] 2차원 배열 순서대로 채우기 1-2.c

--- 1461 : [기초-배열연습] 2차원 배열 순서대로 채우기 1-2.c
+++ 1461 : [기초-배열연습] 2차원 배열 순서대로 채우기 1-2.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int n, n2=1;
+    int n;
     scanf("%d", &n);
     for(int i=0; i<n; i++, puts(""))
-        for(int j=0; j<n; j++, n2--) printf("%d ", (i+1)*n-j);
+        for(int j=0; j<n; j++) printf("%d ", (i+1)*n-j);
         
     return 0;
 }
