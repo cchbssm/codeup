affcf5ea1a8d16f2d6be1bc3dad7a0b06ec4ac26
cchbssm <cheoc222@gmail.com>
2024-09-15 18:35:23 +0900

    finaly i solved the 1861

--- 1861.c
+++ 1861.c
@@ -1,19 +1,32 @@
 //[기초-재귀함수] 파스칼의 삼각형 출력하기 2
 #include <stdio.h>
 
-int pas(int n, int k);
+int pas(int n, int k)
+{
+   if(n < 3 || k == 1 || n == k) return 1;
+   return pas(n-1, k-1) + pas(n-1, k);
+}
+
+void prtcol(int n, int k)
+{
+   if(n < k) return;
+   printf("%d ", pas(n, k));
+   prtcol(n, k+1);
+}
+
+void prt(int n, int k)
+{
+   if(n > k) return;
+   prtcol(n, 1);
+   puts("");
+   prt(n+1, k);
+}
 
 int main()
 {
-   int n, k;
+   int n;
    scanf("%d", &n);
-   printf("%d", pas(n, k));
+   prt(1, n);
 
    return 0;
-}
-
-int pas(int n, int k)
-{
-   if(n < 3 || k == 1 || k == n) return 1;
-   return pas(n-1, k-1) + pas(n-1, k);
 }
